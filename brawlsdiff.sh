dbd807d5332f2a427deb8475f8002c18ccee65bc
libertyernie <maybeway36@gmail.com>
2014-03-24 19:29:12 -0500

    brawlsdiff.sh script updated to take optional width argument

--- brawlsdiff.sh
+++ brawlsdiff.sh
@@ -2,8 +2,19 @@
 # brawlsdiff - shell script to find differences in 2 brawl files.
 # brawlls.exe gets its files from stdin, so it's OK to use cygwin paths if you run from a cygwin shell.
 # Any arguments after the first two will be sent to both instances of brawlls.
+
+width_arg=""
+if [ "$1" = "-w" ];then
+	width_arg="-w $2"
+	if [ "$2" = "full" ];then
+		width_arg="-w $(stty size|awk '{print $2}')"
+	fi
+	shift
+	shift
+fi
 f1="$1"
+f2="$2"
 shift
-f2="$1"
 shift
-sdiff -s <(< "$f1" ~/brawlls.exe -Rm $* -) <(< "$f2" ~/brawlls.exe -Rm $* -)
+brawlls_args="$*"
+sdiff $width_arg -s <(< "$f1" ~/brawlls.exe -Rm $brawlls_args -) <(< "$f2" ~/brawlls.exe -Rm $brawlls_args -)
