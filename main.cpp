db5d4ea5d879739dc0b401fdf474d528191598ed
libertyernie <maybeway36@gmail.com>
2014-03-28 17:00:57 -0500

    cleanup

--- main.cpp
+++ main.cpp
@@ -6,7 +6,5 @@ int compare(array<String^>^ args);
 int main(array<String^>^ args) {
 	if (args->Length > 0 && args[0] == "sdiff") return compare(args);
 
-	String^ file_to_delete = nullptr;
-	int r = brawlls(args);
-	return r;
+	return brawlls(args);
 }
