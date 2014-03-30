2342dcb851f815a4904cd510593ae8881071d988
libertyernie <maybeway36@gmail.com>
2014-03-30 09:43:26 -0500

    Isolate C++/CLI usage in stdt.cpp in an ifdef

--- stdt.cpp
+++ stdt.cpp
@@ -1,11 +1,11 @@
 #include <string>
 #include <cstdio>
 
-using namespace std;
-
+#ifdef __cplusplus_cli
 using System::String;
 using System::Text::StringBuilder;
 using BrawlLib::SSBB::ResourceNodes::ResourceNode;
+#endif
 
 unsigned char safe_c(unsigned char c) {
 	return isprint(c) ? c : ' ';
@@ -15,11 +15,21 @@ union entry_4byte {
 	float f;
 	__int32 i;
 
+	/* Returns a char* pointer to the union's data. */
 	char* ptr() {
 		return (char*)&i;
 	}
 
-	static const size_t SUMMARY_SIZE = 43;
+	/* The size of the summary string written by summary_to_buffer. */
+	static const size_t SUMMARY_SIZE = 42;
+
+	/* Writes a summary string to the memory pointed at by dest, which must have at least SUMMARY_SIZE bytes available.
+	The format of the string is:
+	DDDDDDDDDD / FFFFFFFFFFFF (AAAA) HHHHHHHH
+	DD = integer representation of data
+	FF = floating-point representation of data
+	AAAA = ASCII representation of data (non-printable characters replaced with spaces)
+	HH = data as four hexadecimal bytes */
 	void summary_to_buffer(char* dest) {
 		char* self_ptr = ptr();
 		char as_ascii[4];
@@ -28,13 +38,8 @@ union entry_4byte {
 		}
 		sprintf(dest, "%10d / %12g (%.4s) %08X", i, f, as_ascii, i);
 	}
-	
-	String^ summary_to_cli_str() {
-		char buf[SUMMARY_SIZE];
-		summary_to_buffer(buf);
-		return gcnew String(buf);
-	}
 
+	/* Creates a new entry_4byte union with the order of the bytes reversed. */
 	entry_4byte rv_endian() {
 		entry_4byte output;
 		char* src = ptr();
@@ -53,12 +58,16 @@ bool data_tag_is(const char* tag, ResourceNode^ node) {
 	return true;
 }
 
+#ifdef __cplusplus_cli
 String^ stdt_lines(String^ prefix, ResourceNode^ node) {
 	StringBuilder sb;
 	entry_4byte* addr = (entry_4byte*)(void*)node->UncompressedSource.Address;
-	int length = node->UncompressedSource.Length;
+	int length = node->UncompressedSource.Length / sizeof(entry_4byte);
 	for (int i = 5; i < length; i++) {
-		sb.AppendLine(prefix + "0x" + (i*4).ToString("X3") + ": " + addr[i].rv_endian().summary_to_cli_str());
+		char buf[entry_4byte::SUMMARY_SIZE];
+		addr[i].rv_endian().summary_to_buffer(buf);
+		sb.AppendLine(prefix + "0x" + (i*4).ToString("X3") + ": " + gcnew String(buf));
 	}
 	return sb.ToString();
 }
+#endif
