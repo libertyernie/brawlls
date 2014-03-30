175e3419cfec89a303be852ef09ff5bfe17c0f63
libertyernie <maybeway36@gmail.com>
2014-03-30 10:11:26 -0500

    Detect big/little endian and adjust appropriately

--- stdt.cpp
+++ stdt.cpp
@@ -11,6 +11,14 @@ unsigned char safe_c(unsigned char c) {
 	return isprint(c) ? c : ' ';
 }
 
+bool is_big_endian() {
+	union {
+		unsigned __int32 i;
+		char c[4];
+	} t = { 0x01020304 };
+	return t.c[0] == 1;
+}
+
 union entry_4byte {
 	float f;
 	__int32 i;
@@ -26,17 +34,18 @@ union entry_4byte {
 	/* Writes a summary string to the memory pointed at by dest, which must have at least SUMMARY_SIZE bytes available.
 	The format of the string is:
 	DDDDDDDDDD / FFFFFFFFFFFF (AAAA) HHHHHHHH
-	DD = integer representation of data
-	FF = floating-point representation of data
-	AAAA = ASCII representation of data (opposite endian)
+	DD = integer representation of data (big endian)
+	FF = floating-point representation of data (big endian)
+	AAAA = ASCII representation of data
 	HH = data as four hexadecimal bytes */
 	void summary_to_buffer(char* dest) {
 		char* self_ptr = ptr();
 		char as_ascii[4];
 		for (int z = 0; z < 4; z++) {
-			as_ascii[z] = safe_c(self_ptr[3-z]);
+			as_ascii[z] = safe_c(self_ptr[z]);
 		}
-		sprintf(dest, "%10d / %12g (%.4s) %08X", i, f, as_ascii, i);
+		entry_4byte x = is_big_endian() ? *this : rv_endian();
+		sprintf(dest, "%10d / %12g (%.4s) %08X", x.i, x.f, as_ascii, x.i);
 	}
 
 	/* Creates a new entry_4byte union with the order of the bytes reversed. */
@@ -65,7 +74,7 @@ String^ stdt_lines(String^ prefix, ResourceNode^ node) {
 	int length = node->UncompressedSource.Length / sizeof(entry_4byte);
 	for (int i = 0; i < length; i++) {
 		char buf[entry_4byte::SUMMARY_SIZE];
-		addr[i].rv_endian().summary_to_buffer(buf);
+		addr[i].summary_to_buffer(buf);
 		sb.AppendLine(prefix + "0x" + (i*4).ToString("X3") + ": " + gcnew String(buf));
 	}
 	return sb.ToString();
